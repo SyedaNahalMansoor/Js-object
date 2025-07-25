f07b3c4c76ba3d3cc0dd1d568d4cdb7819a062d9
Syeda Nahal Mansoor <nahalsyeda@gmail.com>
2025-07-25 21:15:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,260 @@
+var students = [
+    {
+        name: "Nahal",
+        class: "10th",
+        fees: 5000
+    },
+    {
+        name: "Sara",
+        class: "9th",
+        fees: 4500
+    },
+    {
+        name: "Ahmed",
+        class: "8th",
+        fees: 4000
+    },
+    {
+        name: "Ayesha",
+        class: "7th",
+        fees: 3500
+    },
+    {
+        name: "Hamza",
+        class: "6th",
+        fees: 3000
+    },
+    {
+        name: "Mariam",
+        class: "5th",
+        fees: 2500
+    },
+    {
+        name: "Bilal",
+        class: "4th",
+        fees: 2000
+    },
+    {
+        name: "Fatima",
+        class: "3rd",
+        fees: 1500
+    },
+    {
+        name: "Usman",
+        class: "2nd",
+        fees: 1200
+    },
+    {
+        name: "Iqra",
+        class: "1st",
+        fees: 1000
+    },
+    {
+        name: "Hassan",
+        class: "10th",
+        fees: 5000
+    },
+    {
+        name: "Zainab",
+        class: "9th",
+        fees: 4500
+    },
+    {
+        name: "Noman",
+        class: "8th",
+        fees: 4000
+    },
+    {
+        name: "Laiba",
+        class: "7th",
+        fees: 3500
+    },
+    {
+        name: "Saad",
+        class: "6th",
+        fees: 3000
+    },
+    {
+        name: "Hina",
+        class: "5th",
+        fees: 2500
+    },
+    {
+        name: "Danish",
+        class: "4th",
+        fees: 2000
+    },
+    {
+        name: "Aiman",
+        class: "3rd",
+        fees: 1500
+    },
+    {
+        name: "Taha",
+        class: "2nd",
+        fees: 1200
+    },
+    {
+        name: "Eman",
+        class: "1st",
+        fees: 1000
+    },
+    {
+        name: "Moiz",
+        class: "10th",
+        fees: 5000
+    },
+    {
+        name: "Sania",
+        class: "9th",
+        fees: 4500
+    },
+    {
+        name: "Shayan",
+        class: "8th",
+        fees: 4000
+    },
+    {
+        name: "Nida",
+        class: "7th",
+        fees: 3500
+    },
+    {
+        name: "Farhan",
+        class: "6th",
+        fees: 3000
+    },
+    {
+        name: "Noor",
+        class: "5th",
+        fees: 2500
+    },
+    {
+        name: "Rehan",
+        class: "4th",
+        fees: 2000
+    },
+    {
+        name: "Mehak",
+        class: "3rd",
+        fees: 1500
+    },
+    {
+        name: "Asim",
+        class: "2nd",
+        fees: 1200
+    },
+    {
+        name: "Rimsha",
+        class: "1st",
+        fees: 1000
+    },
+    {
+        name: "Kashif",
+        class: "10th",
+        fees: 5000
+    },
+    {
+        name: "Sidra",
+        class: "9th",
+        fees: 4500
+    },
+    {
+        name: "Usama",
+        class: "8th",
+        fees: 4000
+    },
+    {
+        name: "Amna",
+        class: "7th",
+        fees: 3500
+    },
+    {
+        name: "Haider",
+        class: "6th",
+        fees: 3000
+    },
+    {
+        name: "Rabia",
+        class: "5th",
+        fees: 2500
+    },
+    {
+        name: "Ibrahim",
+        class: "4th",
+        fees: 2000
+    },
+    {
+        name: "Hoorain",
+        class: "3rd",
+        fees: 1500
+    },
+    {
+        name: "Yousuf",
+        class: "2nd",
+        fees: 1200
+    },
+    {
+        name: "Anaya",
+        class: "1st",
+        fees: 1000
+    },
+    {
+        name: "Shahzaib",
+        class: "10th",
+        fees: 5000
+    },
+    {
+        name: "Kinza",
+        class: "9th",
+        fees: 4500
+    },
+    {
+        name: "Owais",
+        class: "8th",
+        fees: 4000
+    },
+    {
+        name: "Hafsa",
+        class: "7th",
+        fees: 3500
+    },
+    {
+        name: "Talha",
+        class: "6th",
+        fees: 3000
+    },
+    {
+        name: "Zoya",
+        class: "5th",
+        fees: 2500
+    },
+    {
+        name: "Shafiq",
+        class: "4th",
+        fees: 2000
+    },
+    {
+        name: "Alina",
+        class: "3rd",
+        fees: 1500
+    },
+    {
+        name: "Salman",
+        class: "2nd",
+        fees: 1200
+    },
+    {
+        name: "Areeba",
+        class: "1st",
+        fees: 1000
+    }
+]
+
+for (var i = 0; i < students.length; i++) {
+    console.log("Student #" + (i + 1));
+    console.log("Name: " + students[i].name);
+    console.log("Class: " + students[i].class);
+    console.log("Fees: " + students[i].fees);
+    console.log("----------------------");
+}
